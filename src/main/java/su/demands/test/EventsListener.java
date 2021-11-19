cb11f3ea8897cd30533e98e0c43129c4ac29a581
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 23:03:44 +0300

    UPD

--- src/main/java/su/demands/test/EventsListener.java
+++ src/main/java/su/demands/test/EventsListener.java
@@ -1,32 +1,21 @@
 package su.demands.test;
 
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class EventsListener implements Listener {
-    ItemJoin plugin;
 
-    public EventsListener(ItemJoin plugin) {
-
-        this.plugin = plugin;
-    }
+class EventListener implements Listener {
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        PlayerInventory i = player.getInventory();
-        ItemStack emerald = new ItemStack(Material.EMERALD);
-        ItemMeta cmeta = emerald.getItemMeta();
-        cmeta.setDisplayName(ChatColor.AQUA + ("Я ГУЛЬ"));
-        emerald.setItemMeta(cmeta);
-        if (!event.getPlayer().hasPlayedBefore())
-        player.getInventory().addItem(emerald);
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
 
+        ItemStack is = new ItemStack(Material.COMPASS);
+        ItemMeta im = is.getItemMeta();
     }
 }
